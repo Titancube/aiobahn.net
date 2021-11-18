f1372b11eb9673f372339dfcbdb7cbedca1a06eb
Titancube <kbin0717@gmail.com>
2021-11-18 10:21:27 +0900

    Add menu render component

---
+++ components/atoms/link.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-link v-if="isInternal" :to="url">
+    <li>{{ title }}</li>
+  </nuxt-link>
+  <a v-else :href="url" target="_blank" rel="noopener noreferrer">
+    <li>{{ title }}</li>
+  </a>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop(String) title!: string
+  @Prop(String) url!: string
+  @Prop(Boolean) isInternal!: boolean
+
+}
+</script>
+
+<style scoped lang='postcss'>
+</style>
