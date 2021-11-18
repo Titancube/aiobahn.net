05f2ac07852b33392bbf786e0cdeb0147cde0758
Titancube <kbin0717@gmail.com>
2021-11-18 10:21:18 +0900

    Add menu

---
+++ plugins/data.ts
@@ -0,0 +1,12 @@
+export class Generic {
+  static menus: IMenu[] = [
+    { title: 'about', url: '/about', isInternal: true },
+    { title: 'works', url: '/works', isInternal: true },
+    {
+      title: 'access',
+      url: 'https://aiobahn.ffm.to/access',
+      isInternal: false,
+    },
+    { title: 'link', url: '/link', isInternal: true },
+  ]
+}
