74912451952798a49287f8b84dde464e2a8bdf25
Titancube <kbin0717@gmail.com>
2021-11-22 19:06:02 +0900

    Test various env settings

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,9 +1,19 @@
+import path from 'path'
+import dotenv from 'dotenv'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import dotenv from 'dotenv'
-dotenv.config()
+
+dotenv.config({ path: __dirname + '/.env' })
 
 const firebaseConfig = {
+  // apiKey: 'AIzaSyBzBsg7VpKZm8txRWI3ZgbYevMp5jQ4_To',
+  // authDomain: 'aioweb-45ddb.firebaseapp.com',
+  // databaseURL: 'https://aioweb-45ddb.firebaseio.com',
+  // projectId: 'aioweb-45ddb',
+  // storageBucket: 'aioweb-45ddb.appspot.com',
+  // messagingSenderId: '71544283688',
+  // appId: '1:71544283688:web:7a55b22e50dc8e47540b1d',
+
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.FIREBASE_DATABASEURL,
@@ -11,7 +21,6 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
   appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENTID,
 }
 
 const firebaseApp = initializeApp(firebaseConfig)
