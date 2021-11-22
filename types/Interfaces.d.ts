63f9219e7d251daebc1d2a529d01c411c87f91fa
Titancube <kbin0717@gmail.com>
2021-11-22 18:08:19 +0900

    Declare interfaces

--- types/Interfaces.d.ts
+++ types/Interfaces.d.ts
@@ -8,3 +8,10 @@ declare interface INews {
   title: string
   url?: string
 }
+
+declare interface IWork extends INews {}
+declare interface ILink {
+  image: string
+  url: string
+  content: string
+}
